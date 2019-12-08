1fd0132a18eb2a52169202512079e0334290860f
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2019-12-09 00:31:30 +0100

    feat: remoave seconds and adds status as bool; build global array as output

--- fetch-stations.py
+++ fetch-stations.py
@@ -1,9 +1,20 @@
 from elasticsearch import Elasticsearch
 import re
 
+def buildVector(doc):
+  dateExplodeWithoutYear = re.match('[0-9]{4}-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2})', doc['_source']['timestamp']).groups()
+  dateVector = list(map(int, dateExplodeWithoutYear))
+
+  return [
+    doc['_source']['id'],
+    1 if doc['_source']['status'] == 'EN SERVICE' else 0,
+    doc['_source']['bikes'],
+    doc['_source']['docks'],
+  ] + dateVector
+
 es = Elasticsearch(["giskard.aqelia.com"])
 
-res = es.search(index="vlille-stations", body={
+res = es.search(index="vlille-stations", size=10000, body={
   "query": {
     "bool": {
       "must": [
@@ -17,7 +28,8 @@ res = es.search(index="vlille-stations", body={
         {
           "range": {
             "timestamp": {
-              "gte": "2019-12-08T22:20:00",
+              "gte": "2019-10-01",
+              "lte": "2019-10-08",
             }
           }
         }
@@ -27,14 +39,7 @@ res = es.search(index="vlille-stations", body={
 })
 
 print("%d documents found" % res['hits']['total'])
-for doc in res['hits']['hits']:
-  dateExplodeWithoutYear = re.match('[0-9]{4}-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2}):([0-9]{2}).*', doc['_source']['timestamp']).groups()
-  dateVector = list(map(int, dateExplodeWithoutYear))
 
-  vector = [
-    doc['_source']['id'],
-    doc['_source']['bikes'],
-    doc['_source']['docks'],
-  ] + dateVector
+globalSet = list(map(buildVector, res['hits']['hits']))
 
-  print("%s) %s" % (doc['_id'], vector))
+print(globalSet)
