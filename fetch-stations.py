819b3fe23f5c4c9eb3f0ed3593522dd7c38ad7b9
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2019-12-09 22:17:43 +0100

    feat: adds weekday to dataset

--- fetch-stations.py
+++ fetch-stations.py
@@ -1,15 +1,19 @@
 from elasticsearch import Elasticsearch
 import re
+from datetime import datetime
 
 def buildVector(doc):
-  dateExplodeWithoutYear = re.match('[0-9]{4}-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2})', doc['_source']['timestamp']).groups()
+  data = doc['_source']
+
+  dateExplodeWithoutYear = re.match('[0-9]{4}-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2})', data['timestamp']).groups()
   dateVector = list(map(int, dateExplodeWithoutYear))
 
   return [
-    doc['_source']['id'],
-    1 if doc['_source']['status'] == 'EN SERVICE' else 0,
-    doc['_source']['bikes'],
-    doc['_source']['docks'],
+    data['id'],
+    1 if data['status'] == 'EN SERVICE' else 0,
+    data['bikes'],
+    data['docks'],
+    datetime.fromisoformat(data['timestamp']).weekday(),
   ] + dateVector
 
 es = Elasticsearch(["giskard.aqelia.com"])
@@ -40,7 +44,7 @@ res = es.search(index="vlille-stations", size=10000, body={
 def writeToCsv(filename, data):
   with open(filename, 'w') as fp:
     # headers
-    fp.write('id,status,bikes,docks,month,day,hours,minutes' + '\n')
+    fp.write('id,status,bikes,docks,weekday,month,day,hours,minutes' + '\n')
 
     # data
     for vector in data:
@@ -54,4 +58,4 @@ print("%d documents found" % res['hits']['total'])
 globalSet = list(map(buildVector, res['hits']['hits']))
 
 # print(globalSet)
-writeToCsv('dataset/2019-10-01_to_2019-10-31.csv', globalSet)
+writeToCsv('dataset/station-1_2019-10-01_to_2019-10-31.csv', globalSet)
